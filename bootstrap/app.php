77d82e29304611b880f6a880200ec8b81fe6c917
Howka <101456906+HowkaCoder@users.noreply.github.com>
2022-03-16 23:23:19 +0500

    Update app.php

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -25,7 +25,7 @@ $app = new Illuminate\Foundation\Application(
 | incoming requests to this application from both the web and CLI.
 |
 */
-
+  
 $app->singleton(
     Illuminate\Contracts\Http\Kernel::class,
     App\Http\Kernel::class
